4d41cfdeb1586641a61e7aea9c9cbcd791be4a21
KANGUK LEE <iamkanguk97@kakao.com>
2024-03-31 21:59:35 +0900

    [Fix] Q3: Push 메서드 일부 수정

--- push.js
+++ push.js
@@ -20,16 +20,19 @@ console.log(pushV2([1, 2, 3, 4, 5], 6, 7));
 
 /**
  * 하지만 위에처럼 하는건 우리가 실제로 사용하는 배열 메서드 같지 않다.
- * 그래서 우리는 prototype을 사용해야 한다.
+ * 실제 배열의 Push 메서드는 길이를 반환한다.
+ * 그리고 우리는 prototype을 사용해야 한다.
  */
 function pushV3(...items) {
   for (let i = 0; i < items.length; i++) {
     this[this.length] = items[i];
   }
 
-  return this;
+  return this.length;
 }
 
 Array.prototype.pushV3 = pushV3;
 
-console.log([1, 2, 3, 4, 5].pushV3(6, 7));
+const testArray = [1, 2, 3, 4, 5];
+console.log(testArray.pushV3(6, 7));
+console.log(testArray);
